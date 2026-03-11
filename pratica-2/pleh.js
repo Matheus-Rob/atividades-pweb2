3602dc516989ab00be4d6f813daf611f4dbd584f
Matheus Roberto <matheus.roberto@academico.ifpb.edu.br>
2026-03-11 17:23:29 -0300

    ajeitando as pastas

--- pratica-2/pleh.js
+++
@@ -1,8 +0,0 @@
-import { areaCirculo, calc, tipoTriangulo } from "./funcoes.js";
-
-console.log(areaCirculo(6));
-console.log(calc(2, 2, '+'));
-console.log(tipoTriangulo(2, 2, 2));
-console.log(tipoTriangulo(0, 0, -1));
-console.log(tipoTriangulo(4, 3, 4));
-console.log(tipoTriangulo(3, 4, 5))
