a2d3ec73d3bf4cc551baec4568f3b5398d99160c
Matheus Roberto <matheus.roberto@academico.ifpb.edu.br>
2026-03-11 17:20:37 -0300

    arquivos da prática 2

---
+++ pratica-2/pleh.js
@@ -0,0 +1,8 @@
+import { areaCirculo, calc, tipoTriangulo } from "./funcoes.js";
+
+console.log(areaCirculo(6));
+console.log(calc(2, 2, '+'));
+console.log(tipoTriangulo(2, 2, 2));
+console.log(tipoTriangulo(0, 0, -1));
+console.log(tipoTriangulo(4, 3, 4));
+console.log(tipoTriangulo(3, 4, 5))
